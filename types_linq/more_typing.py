e9c58eddc14e039b8a0ce5627f09f20089691c5f
cleoold <cleoold@gmail.com>
2021-01-14 23:05:32 -0500

    fix import

--- types_linq/more_typing.py
+++ types_linq/more_typing.py
@@ -1,13 +1,17 @@
 import sys
-from typing import Any, runtime_checkable
+from typing import Any
 
 if sys.version_info >= (3, 8, 0):
     from typing import Protocol
 else:
-    from typing_extensions import Protocol
+    from typing_extensions import Protocol, runtime_checkable
 
 
 @runtime_checkable
 class SupportsAddAndDiv(Protocol):
     def __add__(self, o: Any) -> Any: ...
     def __truediv__(self, o: Any) -> Any: ...
+
+
+# a type like 'Hashable' is rn useless because a subclass of a hashable class may not
+# be hashable (object -> list)
